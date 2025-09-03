13185649bb6f55e9a40c009815979f75b8e772d4
etramone7-coder <etramone7@gmail.com>
2025-09-04 07:42:50 +0900

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-// config.js
-// CHAYNUPZINE 実行用 GAS URL を固定定義
-window.EXEC_URL = "https://script.google.com/macros/s/AKfycbwOiiXfncZ7n-Ebp9gaCi66ZDOiUqdRbKZSIJhhc0rKiMQqu577CgjQ8_afOQc5md3DZw/exec";

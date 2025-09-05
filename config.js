57fa04f57bbc7e4a6d068eae05ab6aa2b14b6b4d
etramone7-coder <etramone7@gmail.com>
2025-09-05 21:18:31 +0900

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-// config.js
-// GAS の実行URL（GitHub Pages から参照）
-// クエリに ?exec=... が付いていればそちらを優先し、なければ FALLBACK_EXEC を使う
-(function () {
-  var FALLBACK_EXEC =
-    'https://script.google.com/macros/s/AKfycbwOiiXfncZ7n-Ebp9gaCi66ZDOiUqdRbKZSIJhhc0rKiMQqu577CgjQ8_afOQc5md3DZw/exec';
-  var qExec = new URLSearchParams(location.search).get('exec');
-  window.EXEC_URL = qExec || FALLBACK_EXEC;
-})();
